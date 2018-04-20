1e9ebd9a50a1aaafb94a953d7bf38e762ecf75af
harsh <agharsh11@gmail.com>
2018-04-21 01:07:46 +0530

    added the scholastic details

--- greenworld_edutech/serverside/educ8/admin.py
+++ greenworld_edutech/serverside/educ8/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from educ8.models import Student
+from educ8.models import Student,School,ScholasticDetails
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(School)
+admin.site.register(ScholasticDetails)
--- greenworld_edutech/serverside/educ8/models.py
+++ greenworld_edutech/serverside/educ8/models.py
@@ -22,4 +22,19 @@ class School(models.Model):
 	school_name=models.CharField(max_length=20)
 	school_type=models.CharField(max_length=20)
 	study_medium=models.IntegerField()
+	def __str__(self):
+		return "name: "+ self.school_name	
+class ScholasticDetails(models.Model):
+    regular_to_school = models.BooleanField()
+    current_class = models.IntegerField()
+    class_last_year = models.IntegerField()
+    current_school = models.ForeignKey(School, on_delete=models.CASCADE)
+    years_in_current_school = models.IntegerField()
+    current_school_shift = models.IntegerField()
+    is_english_subject = models.BooleanField()
+    break_more_than_three_weeks = models.BooleanField()
+    last_grade_dropout = models.IntegerField()
+    is_private_tuition = models.BooleanField()
+    tuition_fees = models.FloatField()
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
 

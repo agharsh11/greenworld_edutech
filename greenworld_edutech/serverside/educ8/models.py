060f01e6820bde4c940167fdea3dcdab6a1ae439
harsh <agharsh11@gmail.com>
2018-04-21 04:16:11 +0530

    added the json in schools and in the index view too

--- greenworld_edutech/serverside/educ8/models.py
+++ greenworld_edutech/serverside/educ8/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import RegexValidator
 # Create your models here.
+
 class Student(models.Model):
 	first_name=models.CharField(max_length=20)
 	middle_name=models.CharField(max_length=20,blank=True)
@@ -9,21 +10,23 @@ class Student(models.Model):
 	registration_date = models.DateField()
 	year_of_birth=models.IntegerField()
 	birth_date=models.DateField(blank=True)
-	GENDER_CHOICES = (
-        ('M', 'Male'),
-        ('F', 'Female'),
-    )
+	
+	GENDER_CHOICES = (('M', 'Male'),('F', 'Female'),)
 	gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
 	aadhar= models.CharField(max_length=12,blank=True,validators=[RegexValidator(r'^\d{12}$','Number must be 12 digits', 'Invalid aadhaar')])	
 
 	def __str__(self):
 		return "name: "+ self.first_name + " " + self.middle_name + " " + self.last_name
+
 class School(models.Model):
 	school_name=models.CharField(max_length=20)
 	school_type=models.CharField(max_length=20)
 	study_medium=models.IntegerField()
 	def __str__(self):
 		return "name: "+ self.school_name
+	def as_json(self):
+		return dict( school_id = self.id,school_name = self.school_name,school_type=self.school_type,study_medium=self.study_medium)
+
 class ScholasticDetail(models.Model):
     regular_to_school = models.BooleanField()
     current_class = models.IntegerField()

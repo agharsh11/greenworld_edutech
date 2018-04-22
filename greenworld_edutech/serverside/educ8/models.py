bd967bac5f7b99a48892a3edf144096152a0a778
harsh <agharsh11@gmail.com>
2018-04-22 12:43:27 +0530

    added min validator

--- greenworld_edutech/serverside/educ8/models.py
+++ greenworld_edutech/serverside/educ8/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator,MinLengthValidator
 # Create your models here.
 
 class Student(models.Model):
@@ -13,7 +13,7 @@ class Student(models.Model):
 
 	GENDER_CHOICES = (('M', 'Male'),('F', 'Female'),)
 	gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-	aadhar= models.CharField(max_length=12,blank=True,validators=[RegexValidator(r'^\d{12}$','Number must be 12 digits', 'Invalid aadhaar')], unique=True)
+	aadhar= models.CharField(max_length=12,blank=True, unique=True,validators=[MinLengthValidator(12)])#validators=[RegexValidator(r'^\d{12}$','Number must be 12 digits', 'Invalid aadhaar')])
 
 	def __str__(self):
 		return "name: "+ self.first_name + " " + self.middle_name + " " + self.last_name
